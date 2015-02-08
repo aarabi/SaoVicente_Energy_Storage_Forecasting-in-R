e99345c8a4c7cc7eb30e92f5184e9677c3635b9e
Aarabi <aarabi.madhavan@gmail.com>
2015-02-07 19:18:03 -0500

    Updated Monthly , weekly, daily profiles

--- myapp/server.r
+++ myapp/server.r
@@ -4,48 +4,26 @@ library(ggplot2)
 shinyServer(function(input, output)
   {
   
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
+   output$distPlot <- renderPlot({
+     
+     data_list<- c("month","week","day","hour")
+     var1<-data_list[as.numeric(input$select)]
+     
+     var<- get(paste(data_list[as.numeric(input$select)],"_data",sep=""))
+     var$baseload <- input$bins
+     
+     #plot a few graphs
+     ggplot(data=var, aes(x= period, y=Demand)) +
+       geom_line(aes(x=period, y= Demand, color = "Demand"))+
+       geom_line(aes(x= period, y= ifelse(Demand-baseload>Del,Del+baseload,Demand), color = "Wind_Delivered"))+
+       geom_line(aes(x= period, y= baseload, color = "Baseload"))+
+       ylim(c(2000,10000))
+   })
+   
+   output$text1 <- renderText({ 
+     paste("You have selected", input$bins)
+   })
   
-  output$distPlot <- renderPlot(
-    {
-   
-    #bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    week_data$baseload<- input$bins
-    
-   
-    if(input$select=="1")
-    {
-      week_data$curtail[0:9]<- 1500
-      week_data$curtail[10:16]<- 3000
-      week_data$curtail[17:23]<- 2000
-    }
-    else if(input$select=="2")
-    {
-      week_data$curtail<- 2000
-    }
-    else
-    {
-      week_data$curtail<- week_data$Del
-    }
-    
-    print(week_data$curtail)
-    # draw the histogram with the specified number of bins
-    #hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-    #plot a few graphs
-    ggplot(data=week_data, aes(x= 0:23, y=Demand, group=1, factor(hour) )) +
-      geom_line(aes(x= 0:23, y= Demand, color = "Demand"))+
-      geom_line(aes(x= 0:23, y= ifelse(Demand-baseload>Del,Del+baseload,Demand), color = "Del"))+
-      geom_line(aes(x= 0:23, y= baseload, color = "Baseload"))+
-      geom_point(aes(x= 0:23, y= ifelse(Demand-baseload>curtail,curtail+baseload,Demand), color = "Baseload"))+
-      ylim(1000, 10000)
-    
-  }
-  )
+  
+  
 })
--- myapp/ui.r
+++ myapp/ui.r
@@ -9,14 +9,19 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins","Set Baseload Value:",min = 5000,max = 7000,value = 30),
-    
-    selectInput("select", label = h3("Select Set Point"),choices = list("Low,Med,High" = 1, "Med" = 2, "High" = 3),selected = 1)
-    ),
+      selectInput("select", label = h3("Select Graph"),choices = list("Monthly data" = 1, "Weekly data" = 2, "Daily data" = 3,"Hourly data"=4),selected = 1),
+      
+      sliderInput("bins","Set Baseload Value:",min = 5000,max = 7000,value = 30)
+      
+     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      
+      plotOutput("distPlot"),
+      textOutput("text1")
+      
+     
     )
   )
   
